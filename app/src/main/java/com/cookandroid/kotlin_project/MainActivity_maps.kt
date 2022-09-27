389609337919eaf64555db32d3a0a43f62dcf01d
Lee-GS <leegs4831@naver.com>
2022-09-27 16:50:31 +0900

    네이버맵 적용 완료 - 현재위치 띄우기(완료)

--- app/src/main/java/com/cookandroid/kotlin_project/MainActivity_maps.kt
+++ app/src/main/java/com/cookandroid/kotlin_project/MainActivity_maps.kt
@@ -1,14 +1,28 @@
 package com.cookandroid.kotlin_project
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Application
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.location.*
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
 
 class MainActivity_maps : AppCompatActivity(), OnMapReadyCallback {
+
+    var TAG:String = "로그"
+
     private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
 
@@ -18,36 +32,48 @@ class MainActivity_maps : AppCompatActivity(), OnMapReadyCallback {
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient("w1caabbncc")
 
-        locationSource =
-            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        // 뷰 역할을 하는 프래그먼트 객체 얻기
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+
+        // 인터페이스 역할을 하는 NaverMap 객체 얻기
+        // 프래그먼트(MapFragment)의 getMapAsync() 메서드로 OnMapReadyCallback 을 등록하면 비동기로 NaverMap 객체를 얻을 수 있다고 한다.
+        // NaverMap 객체가 준비되면 OnMapReady() 콜백 메서드 호출
+        mapFragment.getMapAsync(this)
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        Log.d(TAG, "MainActivity - onMapReady")
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.uiSettings.isLocationButtonEnabled = true
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<String>,
                                             grantResults: IntArray) {
+        Log.d(TAG, "MainActivity - onRequestPermissionsResult")
         if (locationSource.onRequestPermissionsResult(requestCode, permissions,
                 grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                Log.d(TAG, "MainActivity - onRequestPermissionsResult 권한 거부됨")
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            } else {
+                Log.d(TAG, "MainActivity - onRequestPermissionsResult 권한 승인됨")
+                naverMap.locationTrackingMode = LocationTrackingMode.Follow // 현위치 버튼 컨트롤 활성
+            }
             return
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    override fun onMapReady(naverMap: NaverMap) {
-        this.naverMap = naverMap
-        naverMap.locationSource = locationSource
-        naverMap.locationTrackingMode = LocationTrackingMode.Follow
-
-        val cameraPosition = CameraPosition(
-            LatLng(37.3054, 126.8487),  // 위치 지정
-            16.0 // 줌 레벨
-        )
-        naverMap.cameraPosition = cameraPosition
-    }
-
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
-
-
 
 }
